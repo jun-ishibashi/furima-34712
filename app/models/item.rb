04ca8150d2da285537a3c2266304a00cc57900d9
jun-ishibashi <janvarujan@icloud.com>
2021-06-13 17:14:49 +0900

    商品情報保存機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
   has_one_attached :image
-
+  belongs_to :user
 
   with_options presence: true do
     validates :image
     validates :name
+    validates :description
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid" }
   end
 
@@ -18,7 +19,7 @@ class Item < ApplicationRecord
   with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
-    varidates :payment_id
+    validates :payment_id
     validates :prefecture_id
     validates :shipping_day_id
   end   
